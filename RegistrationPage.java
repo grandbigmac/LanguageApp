ab9892b60fc7315238240f0b93a632a85ad66e6b
grandbigmac <freemirvm@gmail.com>
2022-03-21 17:06:14 +0000

    Changed gui layout to GridBagLayout

--- RegistrationPage.java
+++ RegistrationPage.java
@@ -2,83 +2,102 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.*;
 
 public class RegistrationPage implements ActionListener {
 
     JFrame frame;
     JPanel regPanel;
+    JLabel label1, label2, label3, label4, label5, label6;
+    JTextField nameText, emailText;
+    JPasswordField passwordText, confirmPassText;
+    JButton backButton, submitButton;
 
-    JLabel label1;
-    JLabel label2;
-    JLabel label3;
-    JLabel label4;
-    JLabel label5;
-
-    JTextField nameText;
-    JTextField emailText;
-    JTextField passwordText;
-    JTextField confirmPassText;
-
-    JButton backButton;
-    JButton doneButton;
+    GridBagConstraints gbc = new GridBagConstraints();
 
     public RegistrationPage() {
 
         frame = new JFrame();
 
-        backButton = new JButton("Back");
-        backButton.addActionListener(this);
-        backButton.setBounds(10, 10, 75 , 40);
-
-        label1 = new JLabel("REGISTER", SwingConstants.CENTER);
-        label1.setBounds(0, 50, 400, 60);
-
-        label2 = new JLabel("NAME:");
-        label2.setBounds(20, 100, 200, 60);
-        nameText = new JTextField();
-        nameText.setBounds(20, 140, 350, 20);
-
-        label3 = new JLabel("EMAIL:");
-        label3.setBounds(20, 160, 200, 60);
-        emailText = new JTextField();
-        emailText.setBounds(20, 200, 350, 20);
-
-        label4 = new JLabel("PASSWORD:");
-        label4.setBounds(20, 220, 200, 60);
-        passwordText = new JTextField();
-        passwordText.setBounds(20, 260, 350, 20);
-
-        label5 = new JLabel("CONFIRM PASSWORD:");
-        label5.setBounds(20, 280, 200, 60);
-        confirmPassText = new JTextField();
-        confirmPassText.setBounds(20, 320, 350, 20);
-
-        doneButton = new JButton("SUBMIT");
-        doneButton.addActionListener(this);
-        doneButton.setBounds(120, 380, 100, 40);
-
-
         regPanel = new JPanel();
         regPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        regPanel.setLayout(null);
+        regPanel.setLayout(new GridBagLayout());
 
-        regPanel.add(label1);
-        regPanel.add(label2);
-        regPanel.add(label3);
-        regPanel.add(label4);
-        regPanel.add(label5);
-        regPanel.add(nameText);
-        regPanel.add(emailText);
-        regPanel.add(passwordText);
-        regPanel.add(confirmPassText);
-        regPanel.add(backButton);
-        regPanel.add(doneButton);
+        gbc.insets = new Insets(5, 5, 5, 100);
 
-        frame.add(regPanel, BorderLayout.CENTER);
+        label6 = new JLabel();
+        gbc.gridx = 3;
+        gbc.gridy = 0;
+        regPanel.add(label6, gbc);
+
+        //Back button at top of the page, needs to direct back to the login page when pressed
+        backButton = new JButton("Back");
+        backButton.addActionListener(this);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        regPanel.add(backButton, gbc);
+
+
+        //Title text for the page
+        label1 = new JLabel("REGISTER");
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        regPanel.add(label1, gbc);
+
+        //Field to enter user's name for registration
+        label2 = new JLabel("NAME:");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 3;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        regPanel.add(label2, gbc);
+        nameText = new JTextField();
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        regPanel.add(nameText, gbc);
+
+        //Field to enter user's email address
+        label3 = new JLabel("EMAIL:");
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        regPanel.add(label3, gbc);
+        emailText = new JTextField();
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        regPanel.add(emailText, gbc);
+
+        //Field to create a password for the user's account
+        label4 = new JLabel("PASSWORD:");
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        regPanel.add(label4, gbc);
+        passwordText = new JPasswordField();
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        regPanel.add(passwordText, gbc);
+
+        //Field to confirm the user's password, must match the contents of the previous field
+        label5 = new JLabel("CONFIRM PASSWORD:");
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        regPanel.add(label5, gbc);
+        confirmPassText = new JPasswordField();
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        regPanel.add(confirmPassText, gbc);
+
+        //Submit button, needs to be triggered when user presses enter button
+        submitButton = new JButton("SUBMIT");
+        submitButton.addActionListener(this);
+        gbc.gridx = 1;
+        gbc.gridy = 5;
+        regPanel.add(submitButton, gbc);
+
+
+
+        frame.add(regPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Our GUI");
-        frame.setSize(400, 700);
+        frame.setTitle("Registration Page");
+        frame.pack();
         frame.setVisible(true);
 
     }
