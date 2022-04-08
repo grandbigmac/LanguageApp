4656fa2a65503004db600bd7935239b77e4f97cb
Harry <freemirvm@gmail.com>
2022-04-08 19:47:26 +0100

    Login validation extended to teacher login, and made surrounding guis for navigation

--- TeacherLandingPage.java
+++ TeacherLandingPage.java
@@ -1,69 +1,84 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class TeacherLandingPage {
 
     JFrame frame;
-    JPanel regPanel;
+    JPanel landingPanel;
     JLabel label1, label2, label3, label4, label5, label6;
-    JTextField nameText, emailText;
+    JTextField emailText;
     JPasswordField passwordText;
-    JPasswordField confirmPassText;
-    JButton backButton, submitButton;
+    JButton language1Button;
+    JButton language2Button;
+
     GridBagConstraints gbc = new GridBagConstraints();
 
     public TeacherLandingPage() {
 
         frame = new JFrame();
 
-        regPanel = new JPanel();
-        regPanel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        regPanel.setLayout(new GridBagLayout());
+        landingPanel = new JPanel();
+        landingPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        landingPanel.setLayout(new GridBagLayout());
 
-        gbc.insets = new Insets(5, 5, 5, 100);
+        gbc.insets = new Insets(5, 5, 5, 5);
 
-        label6 = new JLabel();
-        gbc.gridx = 3;
-        gbc.gridy = 0;
-        regPanel.add(label6, gbc);
+        //Image/logo at top of the page
+        try {
+            BufferedImage loginLogo = ImageIO.read(new File("loginpageicon.jpg"));
+            label1 = new JLabel(new ImageIcon(loginLogo));
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            gbc.gridwidth = 2;
+            gbc.fill = GridBagConstraints.CENTER;
+            landingPanel.add(label1, gbc);
+        } catch (IOException e) {
+            System.out.println("Could not find file " + "loginpageicon.jpg");
+        }
 
-        //Back button at top of the page
-        backButton = new JButton("Back");
-        backButton.addActionListener(new ActionListener() {
+        //Title for TeacherLandingPage
+        label2 = new JLabel("Select a language \nto learn today!");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        landingPanel.add(label2, gbc);
+
+        //Button for navigating to first language
+        language1Button = new JButton("Language 1");
+        language1Button.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed (ActionEvent e) {
-                backButton();
+            public void actionPerformed(ActionEvent e) {
+
             }
         });
         gbc.gridx = 0;
-        gbc.gridy = 0;
-        regPanel.add(backButton, gbc);
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        gbc.fill = GridBagConstraints.NONE;
+        landingPanel.add(language1Button, gbc);
 
+        //Button for navigation to second language
+        language2Button = new JButton("Language 2");
+        language2Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-        //Title text for the page
-        label1 = new JLabel("TEACHER LANDING PAGE");
+            }
+        });
         gbc.gridx = 1;
-        gbc.gridy = 0;
-        regPanel.add(label1, gbc);
+        gbc.gridy = 2;
+        gbc.fill = GridBagConstraints.NONE;
+        landingPanel.add(language2Button, gbc);
 
-        frame.add(regPanel);
-        frame.getRootPane().setDefaultButton(submitButton);
+        frame.add(landingPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setTitle("Teacher Landing Page");
+        frame.setTitle("Login Page");
         frame.pack();
         frame.setVisible(true);
-
     }
-
-    public void backButton() {
-        FirstPage r = new FirstPage();
-        frame.dispose();
-    }
-
-
-
 }
-
